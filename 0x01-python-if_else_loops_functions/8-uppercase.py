02797f9fa7c69bcc1bc37213dbf75520afe513d9
kimaita <johnnieqaym@gmail.com>
2023-03-07 15:14:08 +0300

    added task #8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for ch in str:
+        if ord('a') <= ord(ch) <= ord('z'):
+            ch = chr(ord(ch) - 32)
+        print('{}'.format(ch), end='')
+    print()
