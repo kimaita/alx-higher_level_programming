bff8edd24717a1e2519a49865bcdba7a4668f687
kimaita <johnnieqaym@gmail.com>
2023-03-07 15:31:48 +0300

    added task #12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """Prints 1 to 100
+
+    Prints Fizz for multiples of 3, 
+    Buzz for multiples of 5,    
+    FizzBuzz for multiples of both 3 and 5
+    """
+    for i in range(1, 101):
+        if not (i % 3) and not (i % 5):
+            print('FizzBuzz', end=' ')
+        elif not i % 3:
+            print('Fizz', end=' ')
+        elif not i % 5:
+            print('Buzz', end=' ')
+        else:
+            print(i, end=' ')
