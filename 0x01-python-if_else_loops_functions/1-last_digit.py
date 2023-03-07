f35b368f8df762fd4b64cc02483bd3b19146c833
kimaita <johnnieqaym@gmail.com>
2023-03-07 14:16:43 +0300

    fix for division by 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
-last_digit *= number//abs(number)  # retrieve the sign
+if number != 0:
+    last_digit *= number//abs(number)  # retrieve the sign
 is_what = ''
 if last_digit > 5:
     is_what = 'greater than 5'
