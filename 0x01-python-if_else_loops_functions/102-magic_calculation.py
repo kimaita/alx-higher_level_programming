820126a38e623398183ca2cb1264a48095e21c35
kimaita <johnnieqaym@gmail.com>
2023-03-08 10:55:22 +0300

    changed conditional structure

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,8 +2,7 @@
 def magic_calculation(a, b, c):
     if a < b:
         return c
+    elif c > b:
+        return a+b
     else:
-        if c > b:
-            return a+b
-        else:
-            return a*b - c
+        return a*b - c
