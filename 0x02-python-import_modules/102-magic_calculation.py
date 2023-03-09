5e99f1122dba8d17ba8f99ac99af43ad7911b396
kimaita <johnnieqaym@gmail.com>
2023-03-09 16:03:37 +0300

    added advanced task #8

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+import magic_calculation_102
+
+add = magic_calculation_102.add()
+sub = magic_calculation_102.sub()
+
+
+def magic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+    else:
+        return sub(a, b)
+    return None
