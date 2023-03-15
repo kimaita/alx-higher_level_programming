af0eb69b236cdb0dbb73d3adeee4f54ff76a5b75
kimaita <johnnieqaym@gmail.com>
2023-03-15 22:15:14 +0300

    added task #4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    """Returns a set of all elements present in only one set
+
+    Args:
+        set_1: the first set
+        set_2: the second set
+        
+    Returns:
+        The symmetric difference of set_1 and set_2 
+    """
+    return set_1.symmetric_difference(set_2)
