b865bd7a0217fb5a1e7e900440249374e08439bf
kimaita <johnnieqaym@gmail.com>
2023-03-15 22:22:54 +0300

    added task #5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary.
+
+    Args:
+        a_dictionary: the dictionary
+
+    Returns:
+        a count of the dictionary keys
+    """
+    return len(a_dictionary.keys())
