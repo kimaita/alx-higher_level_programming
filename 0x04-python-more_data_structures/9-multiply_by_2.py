66880ae3adfc88693440a7ba678b0cab96fdfb76
kimaita <johnnieqaym@gmail.com>
2023-03-15 22:57:48 +0300

    added task #9

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,12 @@
+# /usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """Multiplies all values of a dictionary by 2
+
+    Args:
+        a_dictionary: the dictionary
+
+    Returns:
+        a new dictionary with all values multiplied by 2
+    """
+    new_dict = dict(map(lambda x: (x[0], x[1]*2), a_dictionary.items()))
+    return new_dict
