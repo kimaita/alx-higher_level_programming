4283a3fc6be4f31f2d13bf7cea66ba4786c82373
kimaita <johnnieqaym@gmail.com>
2023-03-15 23:27:36 +0300

    added task #10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value.
+
+    Args:
+        a_dictionary:the dictionary of scores
+    Returns:
+        Key with the largest integer value or
+        None if no score found
+    """
+    if not a_dictionary:
+        return None
+    sorted_k = sorted(a_dictionary.keys(),
+                      reverse=True,
+                      key=lambda x: a_dictionary.get(x))
+    best = sorted_k[0] or None
+    return best
