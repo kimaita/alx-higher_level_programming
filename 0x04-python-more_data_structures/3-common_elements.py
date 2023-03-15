e7cb81942d4a99577f1eb27bee196d07c2028569
kimaita <johnnieqaym@gmail.com>
2023-03-15 22:05:20 +0300

    added task #3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """Returns a set of common elements in two sets.
+
+    Args:
+        set_1: the first set
+        set_2: the second set
+
+    Returns:
+        intersection of set_1 and set_2
+    """
+    return set_1.intersection(set_2)
