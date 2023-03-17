58be706f0d1a430d4fd33c4f81af34a1aa8eb7bf
kimaita <johnnieqaym@gmail.com>
2023-03-17 20:52:47 +0300

    fixed task #12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-rom_arabic = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
-
-
 def roman_to_int(roman_string):
     """Converts a Roman numeral to an integer.
 
@@ -13,20 +10,20 @@ def roman_to_int(roman_string):
         0 if roman_string not a string or is None
     """
     num = 0
+    rom_arabic = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     if isinstance(roman_string, str):
+        roman_string = roman_string.upper()
         if len(roman_string) == 1:
             return rom_arabic.get(roman_string)
         else:
-            for i in range(1, len(roman_string)):
-                num += action(roman_string[i-1], roman_string[i])
-                i += 2
+            for i in range(0, len(roman_string)-1):
+                right = rom_arabic.get(roman_string[i+1])
+                val = rom_arabic.get(roman_string[i])
+                if val < right:
+                    num -= val
+                else:
+                    num += val
+            num += right
             return num
     else:
         return None
-
-
-def action(x, y):
-    if rom_arabic[x] >= rom_arabic[y]:
-        return rom_arabic[x] + rom_arabic[y]
-
-    return rom_arabic[y] - rom_arabic[x]
