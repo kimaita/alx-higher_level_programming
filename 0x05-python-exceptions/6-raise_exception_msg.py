442ffff17b4f523b732250d2094355c4f627474f
kimaita <johnnieqaym@gmail.com>
2023-03-28 18:04:56 +0300

    added task #6

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """Raises a name exception with a message.
+
+    Args:
+        message: Message to accompany exception. Defaults to "".
+    """
+    raise NameError(message)
