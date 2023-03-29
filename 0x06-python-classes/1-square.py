bec13063af3d799493dc4024cef78382e0c75a68
kimaita <johnnieqaym@gmail.com>
2023-03-29 05:43:06 +0300

    added task #1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square
+    """
+
+    def __init__(self, size):
+        """sets the object's private size attribute on initialization
+
+        Args:
+            size: size of square object
+        """
+        self.__size = size
