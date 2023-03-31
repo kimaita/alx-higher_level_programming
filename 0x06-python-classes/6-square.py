81ea2ce410cc912123c01a226772d923f6ad616b
kimaita <johnnieqaym@gmail.com>
2023-03-31 09:20:05 +0300

    fixed tuple checking

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -51,13 +51,18 @@ class Square:
         """setter for `__position` attribute
 
         Args:
-            value: tuple to set`__position` to
+            value: tuple to set `__position` to
 
         Raises:
             TypeError: if `value` is not a tuple of 2 positive integers
         """
-        if (not isinstance(value, tuple)) or value[0] < 0 or value[1] < 0:
+        if ((not isinstance(value, tuple)) or 
+            len(value) != 2 or
+            (not isinstance(value[0], int)) or 
+            (not isinstance(value[1], int)) or
+            value[0] < 0 or  value[1] < 0):
             raise TypeError('position must be a tuple of 2 positive integers')
+
         self.__position = value
 
     def area(self):
