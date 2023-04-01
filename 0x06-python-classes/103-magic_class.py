0540ce29f0c741e253ece31d8d3ce531f8aca920
kimaita <johnnieqaym@gmail.com>
2023-04-01 20:19:48 +0300

    added default value for radius

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -10,12 +10,12 @@ class MagicClass:
     """Describes a circle with a radius
     """
 
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        else:
-            self.__radius = radius
+        #else:
+        self.__radius = radius
 
     def area(self):
         """Calculates the circle's area."""
