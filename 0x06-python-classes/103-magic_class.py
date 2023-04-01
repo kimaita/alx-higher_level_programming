0d3767793dd92cf37780340cf6ffcc4f4330ab05
kimaita <johnnieqaym@gmail.com>
2023-04-01 20:21:20 +0300

    removed unnecessary else

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -14,7 +14,6 @@ class MagicClass:
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        #else:
         self.__radius = radius
 
     def area(self):
