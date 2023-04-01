2f4e136ddf689f60f496ee2a70772f112c07f348
kimaita <johnnieqaym@gmail.com>
2023-04-01 20:10:39 +0300

    assigned initial __radius value

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module contains a single class `MagicClass`
 
-The class is reverse-engineered fron given Python ByteCode
+The class is reverse-engineered from given Python ByteCode
 """
 
 
@@ -9,15 +9,17 @@ class MagicClass:
     """Describes a circle with a radius
     """
     import math
-    __radius = 0
 
     def __init__(self, radius):
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
+        """Calculates the circle's area."""
         return (self.__radius**2)*math.pi
 
     def circumference(self):
+        """Calculates the circle's circumference."""
         return 2*math.pi*self.__radius
