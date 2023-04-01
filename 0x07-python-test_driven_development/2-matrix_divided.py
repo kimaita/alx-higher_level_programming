eef1beda2c396dd79b22a765ccd72963f6d78014
kimaita <johnnieqaym@gmail.com>
2023-04-02 02:12:33 +0300

    added function for exception raising

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,35 +1,43 @@
 #!/usr/bin/python3
 """This modules defines a function for element-wise division of a matrix
+and a helper function for raising a specific exception with its detail
 """
+
 
 def matrix_divided(matrix, div):
     """Divides each element in a matrix by a number
-    
+
     Args:
         matrix: list of lists of numbers
         div: number to divide matrix elements by
-        
+
     Returns:
         a new matrix of the quotients
     """
     if not isinstance(matrix, list):
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-    
+        raise_matrix_error()
+
     len_first = len(matrix[0])
-        
+
     for row in matrix:
         if not isinstance(row, list) or not len(row):
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            raise_matrix_error()
         if len(row) != len_first:
             raise TypeError('Each row of the matrix must have the same size')
         for i in row:
             if not isinstance(i, int) and not isinstance(i, float):
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
-    
+                raise_matrix_error()
+
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError('div must be a number')
-    
+
     if div:
         return [[round(i/div, 2) for i in row] for row in matrix]
     else:
         raise ZeroDivisionError('division by zero')
+
+
+def raise_matrix_error():
+    """Raises a TypeError."""
+    raise TypeError(
+        'matrix must be a matrix (list of lists) of integers/floats')
