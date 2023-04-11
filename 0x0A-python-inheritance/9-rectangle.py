f24f6b2c292911eacc3790d50c65798aac0da541
kimaita <johnnieqaym@gmail.com>
2023-04-11 16:40:20 +0300

    added task #9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""This module contains a base geometry class and an inherited class"""
+
+
+class BaseGeometry:
+    """A class for input validation"""
+
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Checks if a given argument is an integer greater then 0
+
+        Args:
+            name (str): a name for the `value` argument
+            value (int): the value to validate
+
+        Raises:
+            TypeError: when `value` is not an `int`
+            ValueError: when `value` is not greater than 0
+        """
+        if value.__class__ != int:
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    """Describes a rectangle, with width and height
+    """
+
+    def __init__(self, width, height):
+        """Initializes an instance, setting the width and height
+
+        Args:
+            width (int): rectangle's width
+            height (height): rectangle's height
+        """
+        if super().integer_validator('width', width) is None:
+            self.__width = width
+        if super().integer_validator('height', height) is None:
+            self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
