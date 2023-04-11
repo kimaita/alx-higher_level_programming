6b492b9606895cee54b2f8a19cf8ad367efc6c14
kimaita <johnnieqaym@gmail.com>
2023-04-11 15:23:31 +0300

    added task #6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a single class"""
+
+
+class BaseGeometry:
+    """An unfinished class"""
+
+    def area(self):
+        raise Exception('area() is not implemented')
