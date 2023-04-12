4e1cb33bbff30aeeba81417a73741bd9eaa786e8
kimaita <johnnieqaym@gmail.com>
2023-04-12 21:02:53 +0300

    added new project

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module contains a function for opening and reading files"""
+
+
+def read_file(filename=""):
+    """Reads a file and prints its content to stdout
+
+    Args:
+        filename (str): Name of the fie. Defaults to "".
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read())
