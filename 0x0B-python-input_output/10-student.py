3438b3b4c092fecc0521a13229519a5ac07fa850
kimaita <johnnieqaym@gmail.com>
2023-04-12 23:53:55 +0300

    added task #10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines a class, Student """
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the object's dictionary representation"""
+        if attrs:
+            return {k: val for k, val in self.__dict__.items() if k in attrs}
+        return self.__dict__
