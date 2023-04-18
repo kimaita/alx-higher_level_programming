4cb412d4be163e2832787ad758c2be8b1267557f
kimaita <johnnieqaym@gmail.com>
2023-04-18 03:21:06 +0300

    added tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Unit tests for the Rectangle class"""
-
+from io import StringIO
 import unittest
+from unittest.mock import patch
 from models.rectangle import Rectangle
 
 
@@ -41,3 +42,22 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1.x, 2)
         self.assertEqual(r1.y, 3)
         self.assertEqual(r1.id, 45)
+
+    def test_area(self):
+        """Test the area() method"""
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.area(), 20)
+        r1.width = 5
+        self.assertEqual(r1.area(), 10)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_display(self, mock_print):
+        """Tests the display() method"""
+        r1 = Rectangle(3, 3, 1, 2)
+        r1.display()
+        self.assertEqual('\n\n ###\n ###\n ###\n', mock_print.getvalue())
+
+    def test_str(self):
+        """Test the __str__() method"""
+        r1 = Rectangle(4, 3, 2, 1, 34)
+        self.assertEqual(r1.__str__(), '[Rectangle] (34) 2/1 - 4/3')
