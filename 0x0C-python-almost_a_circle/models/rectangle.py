87df0657b9ce474536732232d86afd9e2245f1f6
kimaita <johnnieqaym@gmail.com>
2023-04-18 03:20:47 +0300

    fixed __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,7 +87,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return the Rectangle instance information"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} -" \
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - " \
             f"{self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
