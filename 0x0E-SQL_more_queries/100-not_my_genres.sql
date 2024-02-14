c956815a9c9d6793f91138322f85aca04fb6e13d
kimaita <johnnieqaym@gmail.com>
2024-02-14 21:15:02 +0300

    refactored query

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,15 +1,18 @@
 -- list all genres not linked to the show Dexter
-WITH dexter_gen AS(
-    SELECT gen.id
-    FROM
-        tv_genres gen
-        JOIN tv_show_genres show_gen ON gen.id = show_gen.genre_id
-        JOIN tv_shows ON show_gen.show_id = tv_shows.id
-        AND tv_shows.title = 'Dexter'
-)
-SELECT name
-FROM tv_genres
+SELECT
+    name
+FROM
+    tv_genres
 WHERE id NOT IN 
-    (SELECT id FROM dexter_gen)
+    (
+        SELECT
+            gen.id
+        FROM
+            tv_genres gen
+            JOIN tv_show_genres show_gen ON gen.id = show_gen.genre_id
+            JOIN tv_shows ON show_gen.show_id = tv_shows.id
+            AND tv_shows.title = 'Dexter'
+    )
 ORDER BY
     name;
+    
