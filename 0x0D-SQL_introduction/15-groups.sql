74ea14a40a3887625e6837dc88607981b9d25232
kimaita <johnnieqaym@gmail.com>
2024-02-15 00:35:51 +0300

    correction

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- lists the number of records with the same score in the table second_table
-SELECT score, COUNT(id) AS number
+SELECT score, COUNT(id) AS 'number'
 FROM second_table
-ORDER BY number DESC;
+ORDER BY 'number' DESC;
