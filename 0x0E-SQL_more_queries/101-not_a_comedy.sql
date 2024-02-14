9ff771f456b6d0f80c40ebf8eb53c24bcf0e3ee2
kimaita <johnnieqaym@gmail.com>
2024-02-14 21:13:18 +0300

    refactored query

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,14 +1,16 @@
 -- lists all shows without the genre Comedy
-WITH comedy_shows AS (
-    SELECT
-        tv.id
-    FROM
-        tv_shows tv
-        JOIN tv_show_genres show_gen ON tv.id = show_gen.show_id
-        JOIN tv_genres gen ON gen.id = show_gen.genre_id
+SELECT
+    title
+FROM
+    tv_shows
+WHERE id NOT IN (
+        SELECT
+            tv.id
+        FROM
+            tv_shows tv
+            JOIN tv_show_genres show_gen ON tv.id = show_gen.show_id
+            JOIN tv_genres gen ON gen.id = show_gen.genre_id
             AND gen.name = 'Comedy'
-)
-SELECT title 
-FROM tv_shows
-WHERE id NOT IN (SELECT id FROM comedy_shows)
-ORDER BY title;
+    )
+ORDER BY
+    title;
