c609f197e3fd5a6ac06515cc54cca3a2693785a4
kimaita <johnnieqaym@gmail.com>
2024-02-15 00:16:50 +0300

    added task #9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,18 @@
+-- creates a table second_table in the database hbtn_0c_0 and adds multiples rows
+-- second_table:
+--    id INT
+--    name VARCHAR(256)
+--    score INT
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO
+    second_table(id, name, score)
+VALUES
+    (1, "John", 10),
+    (1, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
