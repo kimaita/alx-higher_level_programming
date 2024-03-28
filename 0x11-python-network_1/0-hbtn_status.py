b901d4d067c24182326aed136b38178e392d8575
kimaita <johnnieqaym@gmail.com>
2024-03-29 02:57:08 +0300

    added new project

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""fetches an internet resource using urllib displaying the response"""
+import urllib.request
+
+
+def fetch_resource(link):
+    """fetches the internet resource"""
+    req = urllib.request.Request(link)
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print_resource(html)
+
+
+def print_resource(res):
+    """displays info about a fetched resource"""
+    print('Body responese:')
+    print(f"\t- type: {type(res)}")
+    print(f"\t- content: {res}")
+    print(f"\t- utf8 content: {res.decode('utf-8')}")
+
+
+if __name__ == "__main__":
+    fetch_resource("https://alx-intranet.hbtn.io/status")
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,11 @@
+# Python - Network: urllib
+
+We look at how to:
+
+- fetch internet resources with the Python package `urllib`
+- decode `urllib` body response
+- use the Python package `requests`
+- make HTTP `GET` request
+- make HTTP `POST`/`PUT`/etc. request
+- fetch JSON resources
+- manipulate data from an external service
