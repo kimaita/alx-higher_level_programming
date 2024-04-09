9b3eb110589a77eea3a2f094b159cf76bf85b34c
kimaita <johnnieqaym@gmail.com>
2024-04-09 04:49:52 +0300

    modified print method to use variable character

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    c = c || 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
