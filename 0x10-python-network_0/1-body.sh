bd3a253db23b8111535fd0e2633a458dc2f7dc44
kimaita <johnnieqaym@gmail.com>
2024-04-19 16:13:05 +0300

    inlined everything

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Displays the body of an URL request response only if with a 200 status code.
-code=$(curl -sI "$1" | grep HTTP | cut -d' ' -f2)
-
-if ((code == 200)); then
-    curl -s "$1"
-fi
+if (($(curl -sI "$1" | grep HTTP | cut -d' ' -f2) == 200)); then curl -s "$1"; fi;
