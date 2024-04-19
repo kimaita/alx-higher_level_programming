033d9db74b373a87921a92d83555eea9e23ecebb
kimaita <johnnieqaym@gmail.com>
2024-04-20 02:27:13 +0300

    added task #4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches a URL using the requests package"""
+
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    resp = requests.get(url)
+    print('Body response:')
+    print('\t - type:', type(resp.text))
+    print('\t - content:', resp.text)
