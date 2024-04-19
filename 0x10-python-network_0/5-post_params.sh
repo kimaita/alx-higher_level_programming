36c2177a6139a67c4dcf75b12b55f9dcfdee15a5
kimaita <johnnieqaym@gmail.com>
2024-04-19 20:15:24 +0300

    added task #5

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Sends a POST request with the variables 'email' and 'subject' and displays the response
+curl -s -X "POST" -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
