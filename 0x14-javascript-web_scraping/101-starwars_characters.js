6d1a4aff5497fbb333854e831b444b5f331699fa
kimaita <johnnieqaym@gmail.com>
2024-06-06 03:13:00 +0300

    used JS Promise

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,34 @@
 #!/usr/bin/node
+/**
+ * Promise: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+ */
+
 const request = require('request');
 
 const endpoint = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request.get(endpoint, { json: true }, function (error, response, film) {
-  if (error) console.error(error);
-  if (response.statusCode === 200) {
-    const characters = Object.fromEntries(
-      film.characters.map((char) => [char, ''])
+request.get(endpoint, { json: true }, (error, response, film) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const promises = Array.from(
+      film.characters,
+      (character) =>
+        new Promise((resolve, reject) => {
+          request.get(character, { json: true }, (err, resp, actor) => {
+            if (err) {
+              console.error(err);
+            } else {
+              resolve(actor.name);
+            }
+          });
+        })
     );
-    setCharacters(characters);
-  }
-});
 
-function setCharacters (characters) {
-  for (const url of Object.keys(characters)) {
-    request.get(url, { json: true }, (error, response, character) => {
-      if (error) console.error(error);
-
-      if (response.statusCode === 200) characters[url] = character.name;
-
-      if (Object.values(characters).every((x) => x)) {
-        for (const char of Object.values(characters)) {
-          console.log(char);
-        }
-      }
+    Promise.all(promises).then((actorNames) => {
+      actorNames.forEach((entry) => {
+        console.log(entry);
+      });
     });
   }
-}
+});
